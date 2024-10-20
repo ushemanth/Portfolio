30be711b76e2bf4b1386679ab758db704360d6aa
Sai Hemanth Uppala <ushemanth1@gmail.com>
2024-10-20 18:41:41 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const loaderContainer = document.querySelector('.loader-container');
-const redirectUrl = 'https://ushemanth.com'; // Replace with your desired URL
-
-setTimeout(() => {
-  window.location.href = redirectUrl;
-}, 3000); // Adjust the timeout duration as needed (in milliseconds)
